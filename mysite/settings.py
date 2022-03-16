1aac2727da99c9f2b5fd264b605d04aaa8cab456
mijiFernandes <lkm366859@gmail.com>
2022-03-16 22:08:07 +0900

    Added files and changes required for deployment to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SAMESITE = 'None'
