38a5775064699d86a09a8a10aa88ee0e08418cfb
mijiFernandes <lkm366859@gmail.com>
2022-03-16 23:13:36 +0900

    Added files and changes required for deployment to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ihu%bblzs)g92j)u-qx3iq$!ss@e=!ac$gk+t2x6=-cd9u9@%'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-%ihu%bblzs)g92j)u-qx3iq$!ss@e=!ac$gk+t2x6=-cd9u9@%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
